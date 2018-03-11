3238ea25a1391b82a34d45cca623cfb2ce5c54cf
gabrielavmattos <gmattos@thoughtworks.com>
2018-03-10 22:33:50 -0300

    Insert ansible to setup programs mac

---
+++ ansible/install-requirements-mac.sh
@@ -0,0 +1,6 @@
+# Install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install ansible
+sudo easy_install pip
+sudo pip install ansible
